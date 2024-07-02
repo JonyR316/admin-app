ffc1e0a1c7ccc7f5349d19f4d9ca50398dff4d9c
JonyR316 <jlc.rodriguez316@gmail.com>
2024-07-02 06:40:27 -0500

    Antes de abrir la cuenta de amazon

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -9,6 +9,7 @@ export default function ProductForm({
   title: existingTitle,
   descripcion: existingDescripcion,
   precio: existingPrecio,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [descripcion, setDescripcion] = useState(existingDescripcion || "");
@@ -28,6 +29,21 @@ export default function ProductForm({
   if (goToProducts) {
     router.push("/products");
   }
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: data,
+      });
+
+      console.log(res);
+    }
+  }
   return (
     <form onSubmit={saveProduct}>
       <label>NOMBRE PRODUCTO</label>
@@ -37,6 +53,28 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>IMAGENES</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-900 rounded-lg bg-gray-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m0-3-3-3m0 0-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75"
+            />
+          </svg>
+          <div>CARGAR</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+        {!images?.length && <div>NO EXISTEN IMAGENES DEL PRODUCTO</div>}
+      </div>
       <label>DESCRIPCION</label>
       <textarea
         placeholder="descripcion"
